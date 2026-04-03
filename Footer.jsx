f7abe2ecf8215bae6a2b92619a5a93b90d01a4c3
kanakparasar42-del <kanakparasar42@gmail.com>
2026-04-03 13:52:47 +0530

    Adding Footer

---
+++ Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <h2>AI Fitness</h2>
+        <p>Train smarter with AI-powered posture detection</p>
+
+        <div className="footer-links">
+          <a href="#">Home</a>
+          <a href="#">Workout</a>
+          <a href="#">Dashboard</a>
+        </div>
+
+        <p className="copyright">
+          © 2026 AI Fitness Project | Developed by Kanak Parasar
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
